e0e549f19fa9d066df547fb7d3a90842278a9b2c
Homework-10 <danilovsansan@gmail.com>
2025-04-20 07:35:16 +0300

    Initial Commit

--- src/org/skypro/skyshop/App.java
+++ src/org/skypro/skyshop/App.java
@@ -8,17 +8,17 @@ import java.util.Arrays;
 import static org.skypro.skyshop.basket.ProductBasket.getRepositoryProduct;
 
 public class App {
+
     public static void main(String[] args) {
 
-
-////     ProductBasket.addProduct(new Product("robot", 10_000));
-////        ProductBasket.addProduct(new Product("Boock", 22));
-////        ProductBasket.addProduct(new Product("Компьютер", 1_000));
-////        ProductBasket.addProduct(new Product("Машина", 1_000_000));
-////        ProductBasket.addProduct(new Product("Еда", 1_000_000));
-////        ProductBasket.addProduct(new Product("Еда", 1_000_000));
-////        ProductBasket.summationTheTotalPraiceTheBasket();
-//        ProductBasket.printPraiceBasket();
+        ProductBasket.addProduct(new Product("robot", 10_000));
+        ProductBasket.addProduct(new Product("Boock", 22));
+        ProductBasket.addProduct(new Product("Компьютер", 1_000));
+        ProductBasket.addProduct(new Product("Машина", 1_000_000));
+        ProductBasket.addProduct(new Product("Еда", 1_000_000));
+        ProductBasket.addProduct(new Product("Еда", 1_000_000));
+        ProductBasket.summationTheTotalPraiceTheBasket();
+        ProductBasket.printPraiceBasket();
         System.out.println(Arrays.toString(getRepositoryProduct()));
     }
 
